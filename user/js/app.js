e11cb570c4dbb2a405b409b502bfed162cf83120
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-12-27 22:13:42 -0600

    add news reader

---
+++ user/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.controllers',
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view/:title', {templateUrl: 'partials/view.html', controller: 'ViewCtrl'});
+}]);
