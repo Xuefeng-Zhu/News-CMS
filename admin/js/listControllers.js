ca28428d4d6d449eb0a4718492d0f18fa5cb40b6
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-01 22:33:25 -0600

    add news list search

--- admin/js/listControllers.js
+++ admin/js/listControllers.js
@@ -5,6 +5,26 @@
 angular.module('listControllers', []).
 controller('ListCtrl', ['$scope', '$http',
     function($scope, $http) {
- 
+        $scope.page = 0;
+        $scope.tags = []
+
+        $scope.searchNews = function() {
+            var data = {
+                search: $scope.search,
+                tags: [],
+                page: $scope.page
+            }
+
+            angular.forEach($scope.tags, function(tag) {
+                    data.tags.push(tag.name);
+            })
+
+            $http.post(url + '/search_news', data)
+                .success(function(response) {
+                    $scope.newsList = response;
+                })
+        }
+
+        $scope.searchNews();
     }
 ]);
