49a0111d51f5e42e6c776f6a0ada5280f1d69cde
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-06-07 22:57:21 -0700

    remove everything you need dir

--- everything you need/model/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask.ext.mongoengine import MongoEngine
-from flask_redis import Redis
-from flask.ext.bcrypt import Bcrypt
-
-redis_store = Redis()
-db = MongoEngine()
-bcrypt = Bcrypt()
--- everything you need/model/comment.py
+++
@@ -1,8 +0,0 @@
-from model import db
-from datetime import datetime
-
-
-class Comment(db.EmbeddedDocument):
-    username = db.StringField(required=True)
-    content = db.StringField(required=True)
-    date = db.DateTimeField(default=datetime.now)
